e888ad8bc5cf6cb43bdc0897027635e947d51a0f
Harsh Makhija <harsh.makhija24@iimb.ac.in>
2026-08-05 10:41:48 +0530

    chore: ponytail mode - removed all one-off scratch scripts to reduce clutter

--- scratch/fix_links.py
+++
@@ -1,8 +0,0 @@
-import os, glob
-for f in glob.glob('**/*.html', recursive=True):
-    with open(f, 'r', encoding='utf-8') as file:
-        content = file.read()
-    if 'homepage_v4_stitch.html#about' in content:
-        content = content.replace('homepage_v4_stitch.html#about', 'about_v1.html')
-        with open(f, 'w', encoding='utf-8') as file:
-            file.write(content)
